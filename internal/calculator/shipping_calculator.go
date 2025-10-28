920fde15d9898148528e4d1a9753ed4ecffa32b7
SanhapojA <sanhapoj.api@xspringas.com>
2025-10-28 15:53:00 +0700

    add feature calulate

--- internal/calculator/shipping_calculator.go
+++ internal/calculator/shipping_calculator.go
@@ -1,6 +1,8 @@
 package calculator
 
-import "shipping-calculator/internal/domain"
+import (
+	"shipping-calculator/internal/domain"
+)
 
 type ShippingCalculator struct{}
 
@@ -10,17 +12,63 @@ func NewShippingCalculator() *ShippingCalculator {
 
 func (c *ShippingCalculator) Calculate(shipment *domain.Shipment) (float64, error) {
 	weight := shipment.Weight
+	distance := shipment.Distance
 
+	// 1) compute base fee from weight
+	var baseFee float64
 	switch {
 	case weight > 0 && weight <= 1:
-		return 30.0, nil
+		baseFee = 30.0
 	case weight > 1 && weight <= 5:
-		return 50.0, nil
+		baseFee = 50.0
 	case weight > 5 && weight <= 10:
-		return 80.0, nil
+		baseFee = 80.0
 	case weight > 10:
-		return 80.0 + (weight-10)*10, nil
+		baseFee = 80.0 + (weight-10.0)*10.0
 	default:
 		return 0, nil
 	}
+
+	// 2) compute distance multiplier: 0-99km -> 1.0, 100-199 -> 1.5, 200-299 -> 2.0, ...
+	multiplier := 1.0
+	if distance > 0 && distance <= 50 {
+		multiplier = 1.0
+	} else if distance > 50 && distance <= 200 {
+		multiplier = 1.5
+	} else {
+		multiplier = 2.0
+	}
+	deliveryType := shipment.DeliveryType
+	additionalWithDeliveryType := CalculateWithDeliveryType(deliveryType)
+
+	fee := baseFee * multiplier * additionalWithDeliveryType
+
+	fee, err := CalculateWithPromotionCode(fee, string(shipment.PromoCode))
+	if err != nil {
+		return 0, err
+	}
+	return fee, nil
+}
+
+func CalculateWithDeliveryType(deliveryType domain.DeliveryType) float64 {
+	if deliveryType == domain.DeliveryTypeExpress {
+		return 1.5
+	}
+	return 1.0
+}
+
+func CalculateWithPromotionCode(baseFee float64, promoCode string) (float64, error) {
+	fee := baseFee
+	if promoCode != "" {
+		discount := PromotionDiscounts[promoCode]
+		if discount == 0 {
+			return 0, domain.ErrInvalidPromoCode
+		}
+		fee = fee - discount
+		if fee < 0 {
+			fee = 0
+		}
+	}
+
+	return fee, nil
 }
--- internal/calculator/shipping_calculator_test.go
+++ internal/calculator/shipping_calculator_test.go
@@ -82,3 +82,261 @@ func TestWeightBasedPricing(t *testing.T) {
 	})
 
 }
+
+func TestShippingFeeCalculator(t *testing.T) {
+	t.Run("Calculate distance 30 km and weight 0.5 should be fee 30 THB", func(t *testing.T) {
+		calc := NewShippingCalculator()
+		shipment := &domain.Shipment{
+			Weight:       0.5,
+			Distance:     30,
+			DeliveryType: domain.DeliveryTypeStandard,
+		}
+
+		fee, err := calc.Calculate(shipment)
+
+		assert.NoError(t, err)
+		assert.Equal(t, 30.0, fee)
+	})
+
+	t.Run("Calculate distance 50 km and weight 1.5 should be fee 50 THB", func(t *testing.T) {
+		calc := NewShippingCalculator()
+		shipment := &domain.Shipment{
+			Weight:       1.5,
+			Distance:     50,
+			DeliveryType: domain.DeliveryTypeStandard,
+		}
+
+		fee, err := calc.Calculate(shipment)
+
+		assert.NoError(t, err)
+		assert.Equal(t, 50.0, fee)
+	})
+
+	t.Run("Calculate distance 90 km and weight 5.5 should be fee 120 THB", func(t *testing.T) {
+		calc := NewShippingCalculator()
+		shipment := &domain.Shipment{
+			Weight:       5.5,
+			Distance:     90,
+			DeliveryType: domain.DeliveryTypeStandard,
+		}
+
+		fee, err := calc.Calculate(shipment)
+
+		assert.NoError(t, err)
+		assert.Equal(t, 120.0, fee)
+	})
+
+	t.Run("Calculate distance 160 km and weight 12.0 should be fee 150 THB", func(t *testing.T) {
+		calc := NewShippingCalculator()
+		shipment := &domain.Shipment{
+			Weight:       12.0,
+			Distance:     160,
+			DeliveryType: domain.DeliveryTypeStandard,
+		}
+
+		fee, err := calc.Calculate(shipment)
+
+		assert.NoError(t, err)
+		assert.Equal(t, 150.0, fee)
+	})
+
+	t.Run("Calculate distance 200 km and weight 15.0 should be fee 195 THB", func(t *testing.T) {
+		calc := NewShippingCalculator()
+		shipment := &domain.Shipment{
+			Weight:       15.0,
+			Distance:     200,
+			DeliveryType: domain.DeliveryTypeStandard,
+		}
+
+		fee, err := calc.Calculate(shipment)
+
+		assert.NoError(t, err)
+		assert.Equal(t, 195.0, fee)
+	})
+
+	t.Run("Calculate distance 500 km and weight 15.0 should be fee 260 THB", func(t *testing.T) {
+		calc := NewShippingCalculator()
+		shipment := &domain.Shipment{
+			Weight:       15.0,
+			Distance:     500,
+			DeliveryType: domain.DeliveryTypeStandard,
+		}
+
+		fee, err := calc.Calculate(shipment)
+
+		assert.NoError(t, err)
+		assert.Equal(t, 260.0, fee)
+	})
+}
+
+func TestDeliveryType(t *testing.T) {
+	t.Run("Calculate fee based on delivery type is standard, weight is 0.5, distance is 30 should be fee 30 THB", func(t *testing.T) {
+		calc := NewShippingCalculator()
+		shipment := &domain.Shipment{
+			Weight:       0.5,
+			Distance:     30,
+			DeliveryType: domain.DeliveryTypeStandard,
+		}
+
+		fee, err := calc.Calculate(shipment)
+		assert.NoError(t, err)
+		assert.Equal(t, 30.0, fee)
+	})
+
+	t.Run("Calculate fee based on delivery type is standard, weight is 5.5, distance is 90 should be fee 120 THB", func(t *testing.T) {
+		calc := NewShippingCalculator()
+		shipment := &domain.Shipment{
+			Weight:       5.5,
+			Distance:     90,
+			DeliveryType: domain.DeliveryTypeStandard,
+		}
+
+		fee, err := calc.Calculate(shipment)
+		assert.NoError(t, err)
+		assert.Equal(t, 120.0, fee)
+	})
+
+	t.Run("Calculate fee based on delivery type is standard, weight is 15.0, distance is 200 should be fee 195 THB", func(t *testing.T) {
+		calc := NewShippingCalculator()
+		shipment := &domain.Shipment{
+			Weight:       15.0,
+			Distance:     200,
+			DeliveryType: domain.DeliveryTypeStandard,
+		}
+
+		fee, err := calc.Calculate(shipment)
+		assert.NoError(t, err)
+		assert.Equal(t, 195.0, fee)
+	})
+
+	t.Run("Calculate fee based on delivery type is express, weight is 1.5, distance is 50 should be fee 75 THB", func(t *testing.T) {
+		calc := NewShippingCalculator()
+		shipment := &domain.Shipment{
+			Weight:       1.5,
+			Distance:     50,
+			DeliveryType: domain.DeliveryTypeExpress,
+		}
+
+		fee, err := calc.Calculate(shipment)
+		assert.NoError(t, err)
+		assert.Equal(t, 75.0, fee)
+	})
+
+	t.Run("Calculate fee based on delivery type is express, weight is 12.0, distance is 160 should be fee 225 THB", func(t *testing.T) {
+		calc := NewShippingCalculator()
+		shipment := &domain.Shipment{
+			Weight:       12.0,
+			Distance:     160,
+			DeliveryType: domain.DeliveryTypeExpress,
+		}
+
+		fee, err := calc.Calculate(shipment)
+		assert.NoError(t, err)
+		assert.Equal(t, 225.0, fee)
+	})
+
+	t.Run("Calculate fee based on delivery type is express, weight is 15.0, distance is 500 should be fee 390 THB", func(t *testing.T) {
+		calc := NewShippingCalculator()
+		shipment := &domain.Shipment{
+			Weight:       15.0,
+			Distance:     500,
+			DeliveryType: domain.DeliveryTypeExpress,
+		}
+
+		fee, err := calc.Calculate(shipment)
+		assert.NoError(t, err)
+		assert.Equal(t, 390.0, fee)
+	})
+}
+
+func TestCalculateWithPromotion(t *testing.T) {
+	t.Run("Calculate fee base on promotion code with weight is 0.5 and distance is 30 then fee should be 0", func(t *testing.T) {
+		calc := NewShippingCalculator()
+		shipment := &domain.Shipment{
+			Weight:       0.5,
+			Distance:     30,
+			DeliveryType: domain.DeliveryTypeStandard,
+			PromoCode:    "FREESHIP50",
+		}
+
+		fee, err := calc.Calculate(shipment)
+		assert.NoError(t, err)
+		assert.Equal(t, 0.0, fee)
+	})
+
+	t.Run("Calculate fee base on promotion code with weight is 1.5 and distance is 50 then fee should be 0", func(t *testing.T) {
+		calc := NewShippingCalculator()
+		shipment := &domain.Shipment{
+			Weight:       1.5,
+			Distance:     50,
+			DeliveryType: domain.DeliveryTypeStandard,
+			PromoCode:    "FREESHIP50",
+		}
+
+		fee, err := calc.Calculate(shipment)
+		assert.NoError(t, err)
+		assert.Equal(t, 0.0, fee)
+	})
+
+	t.Run("Calculate fee base on promotion code with weight is 5.5 and distance is 30 then fee should be 30", func(t *testing.T) {
+		calc := NewShippingCalculator()
+		shipment := &domain.Shipment{
+			Weight:       5.5,
+			Distance:     30,
+			DeliveryType: domain.DeliveryTypeStandard,
+			PromoCode:    "FREESHIP50",
+		}
+
+		fee, err := calc.Calculate(shipment)
+		assert.NoError(t, err)
+		assert.Equal(t, 30.0, fee)
+	})
+
+	t.Run("Calculate fee base on promotion code with weight is 12.0 and distance is 160 then fee should be 50", func(t *testing.T) {
+		calc := NewShippingCalculator()
+		shipment := &domain.Shipment{
+			Weight:       12.0,
+			Distance:     160,
+			DeliveryType: domain.DeliveryTypeStandard,
+			PromoCode:    "FREESHIP100",
+		}
+
+		fee, err := calc.Calculate(shipment)
+		assert.NoError(t, err)
+		assert.Equal(t, 50.0, fee)
+	})
+
+	t.Run("Calculate fee base on promotion code with weight is 15.0 and distance is 200 then fee should be 95", func(t *testing.T) {
+		calc := NewShippingCalculator()
+		shipment := &domain.Shipment{
+			Weight:       15.0,
+			Distance:     200,
+			DeliveryType: domain.DeliveryTypeStandard,
+			PromoCode:    "FREESHIP100",
+		}
+
+		fee, err := calc.Calculate(shipment)
+		assert.NoError(t, err)
+		assert.Equal(t, 95.0, fee)
+	})
+
+	t.Run("Calculate fee error with invalid promo code", func(t *testing.T) {
+		calc := NewShippingCalculator()
+		shipment := &domain.Shipment{
+			Weight:       5.5,
+			Distance:     90,
+			DeliveryType: domain.DeliveryTypeStandard,
+			PromoCode:    "FREESHIP250",
+		}
+
+		_, err := calc.Calculate(shipment)
+		assert.Error(t, err)
+
+	})
+}
+
+func TestCalculateWithPromotionOrderAmountDiscount(t *testing.T) {
+	t.Run("Calculate with order amount is 1500 and weight 0.5 and distance 30 should be fee 24 thb", func(t *testing.T) {
+
+	})
+}
